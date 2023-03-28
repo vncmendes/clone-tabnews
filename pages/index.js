b7682deb46b13e2097a381f5b8924254c5d0990f
Vinicius Mendes <viniciussmendes@gmail.com>
2023-03-28 16:33:20 +0000

    fix error

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Home() {
                 <h1>Você é o elétron que faltava no meu proton</h1>
                 <h1>e a nossa união criou o nucleo atômico</h1>
                 <h1>a força forte</h1>
-                <h2>*-*</h2>
+                <h1>*-*</h1>
             </Container>
             <Container>
                 <img src="https://cdn.pixabay.com/animation/2023/01/08/01/22/01-22-40-685_512.gif" />
