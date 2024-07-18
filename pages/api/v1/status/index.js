167a6fc39ff4234adf9501222ea2195bf8716abe
Vini Mendes <viniciussmendes@gmail.com>
2024-07-18 22:41:40 +0000

    finish the `endpoints / status`and improve `tests`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,34 @@
-import database, { databaseInformations } from "/infra/database.js";
+import database from "/infra/database.js";
+
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const dbInfoResponse = await databaseInformations();
-  console.log(dbInfoResponse);
-  
+  const maxConnection = await database.query("SHOW MAX_CONNECTIONS;");
+  const polishedMaxConnections = maxConnection.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB
+  console.log(databaseName);
+  const openedConnections = await database.query({text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", values: [databaseName]});
+
+  console.log(openedConnections.rows[0].count);
+  const polishedOpenedConnections = openedConnections.rows[0].count
+
+  const latency = await database.query("SELECT 2 + 5");
+
+  const version = await database.query("SHOW SERVER_VERSION;");
+  const polishedVersion = version.rows[0].server_version;
+
+
   response.status(200).json({
-    updated_at: updatedAt
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(polishedMaxConnections),
+        opened_connections: polishedOpenedConnections,
+        latency: latency,
+        version: polishedVersion
+      }
+    }
   });
 }
 
--- test/integration/v1/status/get.test.js
+++ test/integration/v1/status/get.test.js
@@ -9,5 +9,9 @@ describe("Teste de conexão com o servidor WEB", () => {
 
     const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
     expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
   })
 })
