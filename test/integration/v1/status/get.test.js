3e147d3d01cc2495df8474bfec9a0f1479e25769
Vini Mendes <viniciussmendes@gmail.com>
2024-07-23 22:07:36 +0000

    catching the error by try catch

--- test/integration/v1/status/get.test.js
+++ test/integration/v1/status/get.test.js
@@ -1,6 +1,6 @@
 describe("Teste de conexão com o servidor WEB", () => {
   test("GET to /api/v1/status should return 200 Status Code", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+    const response = await fetch("http://localhost:3001/api/v1/status");
     expect(response.status).toBe(200);
 
     const responseBody = await response.json();
