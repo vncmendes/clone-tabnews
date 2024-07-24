187332d859664a3fd9f4b6097d91e7a2a98fc566
Vini Mendes <viniciussmendes@gmail.com>
2024-07-24 19:45:42 +0000

    getting  ,

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,14 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true
+    ssl: process.env.NODE_ENV === "development" ? false : true
   })
 
   console.log("postgres enviroment variables", {
@@ -15,7 +16,8 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV
   });
 
   try {
